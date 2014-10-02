a4f06401499d3ac70385c1c8690e44cf62ce27fd
lmsol1030 <lmsol1030@naver.com>
2014-10-02 16:52:45 +0900

    1

--- sketch.js
+++ sketch.js
@@ -23,11 +23,11 @@ if (mouseX < 300) {
 if (mouseX < 100) {
   ellipse(50, 200, 100, 100);
 } else if (mouseX < 200){
-  ellipse(150, 200, 100, 100);
+  rect(150, 200, 100, 100);
 } else if (mouseX < 300){
   ellipse(250, 200, 100, 100);
 } else if (mouseX < 400){
-  ellipse(350, 200, 100, 100);
+  rect(350, 200, 100, 100);
 } else if (mouseX < 500){
   ellipse(450, 200, 100, 100);
 } else {
