31deb0fa54d4fe2443acc5df7dbf239475fc3c81
lmsol1030 <lmsol1030@naver.com>
2014-10-02 16:18:57 +0900

    1

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup()
+ {
+  createCanvas(800, 600);
+  noFill();
+  stroke(255, 0, 0);
+}
+
+  function draw()
+{
+  background(0);
+  if (mouseX < 300) {
+  ellipse(200,200,100,100);
+}
+}
+  
