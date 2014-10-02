56281ae39719a8280d34e4424ed71d041eb39375
lmsol1030 <lmsol1030@naver.com>
2014-10-02 16:33:51 +0900

    s

--- sketch.js
+++ sketch.js
@@ -18,4 +18,19 @@ if (mouseX < 300) {
 } else {
   ellipse(400, 200, 100, 100);
 }
+
+// draw function
+if (mouseX < 100) {
+  ellipse(50, 200, 100, 100);
+} else if (mouseX < 200){
+  ellipse(150, 200, 100, 100);
+} else if (mouseX < 300){
+  ellipse(250, 200, 100, 100);
+} else if (mouseX < 400){
+  ellipse(350, 200, 100, 100);
+} else if (mouseX < 500){
+  ellipse(450, 200, 100, 100);
+} else {
+  ellipse(550, 200, 100, 100);
+}
 }
